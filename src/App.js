784970a0a45a7374a965d4c417d1460a2d68a56d
chaitali-gaikwad <chaitali.g.gaikwad@gmail.com>
2024-02-10 16:34:25 +0530

    initial update

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Chaitali here, this is my demo for Exp5 DevOps.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello World
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
